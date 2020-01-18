2380a499ac2c99b90003ff398d3aa0c85aad52d1
devesh <devesh47cool@gmail.com>
2020-01-19 00:45:37 +0530

    initial card for weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
 import logo from './logo.svg';
-import './App.css';
-import {Row, Container, Nav, Jumbotron} from "react-bootstrap";
+import './App.scss';
+import {Row, Container, Nav, Jumbotron, Card, ListGroup,ListGroupItem} from "react-bootstrap";
 
-function App() {
+// weather update for one day
+class WeatherCard extends Component {
+  render() {
+    return <Card style={{width: '18rem'}}>
+        <Card.Img variant="top" src=""/>
+        <Card.Body>
+         <Card.Title>Wed</Card.Title>
+         <ListGroup className="list-group-flush">
+           <ListGroupItem>34 </ListGroupItem>
+           <ListGroupItem>23 </ListGroupItem>
+         </ListGroup>
+        </Card.Body>
+    </Card>
+  }
+}
+
+
+
+
+
+
+
+class App extends Component {
+
+  render() {
   return (
-   <Container>
+   <Container className="App">
      <Row className="header">
          <Nav activekey="/home"
               onSelect={selectedKey => alert(`selected ${selectedKey}`)}
@@ -18,15 +42,20 @@ function App() {
          </Nav>
 
      </Row>
-     <Row>
-         <Jumbotron className="justify-content-md-center" fluid>
+     <Row id="content">
+         <Jumbotron style={{width: '90%'}} className="justify-content-md-center" fluid>
              <h1> Weather man forecast!</h1>
+
          </Jumbotron>
+         <Container>
+          <WeatherCard> </WeatherCard>
+         </Container>
 
 
      </Row>
    </Container>
   );
+}
 }
 
 export default App;
