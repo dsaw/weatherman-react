ca21da4c7a412975d336061f26ad19f3becfefc3
devesh <devesh47cool@gmail.com>
2020-04-19 22:02:08 +0530

    Set up css modules for autosuggest

--- src/App.js
+++ src/App.js
@@ -20,24 +20,13 @@ class App extends Component {
   return (
    <Container className="App">
      <Row className="header">
-         <Nav variant="pills" activekey="/home"
-              onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-              >
-             <Nav.Item>
-                 <Nav.Link href="/about"> About </Nav.Link>
-             </Nav.Item>
-         </Nav>
-         <Col>
+         <Col sm={true} md={8} lg={6}>
            <SearchInput />
          </Col>
 
 
      </Row>
      <Row id="content">
-         <Jumbotron style={{width: '90%'}} className="justify-content-md-center" fluid>
-             <h1> Weather man forecast!</h1>
-
-         </Jumbotron>
          <Row className="justify-content-md-space-between">
           <Col><WeatherCard day="Wednesday" highestTemp={35} lowestTemp={34} weatherType="cloudy" humidity={21} speed={2}> </WeatherCard></Col>
           <Col><WeatherCard day="Monday" highestTemp={25} lowestTemp={24} weatherType="sunny" humidity={21} speed={2}> </WeatherCard></Col>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,6 +3,7 @@ import Search from 'react-search';
 import Autosuggest from 'react-autosuggest';
 import * as cityListConfig from '../../data/city.list.json';
 
+import theme from './Search.module.scss';
 
 // for renaming name to id of city list
 function renameKeys(obj, newKeys) {
@@ -91,9 +92,9 @@ class SearchInput extends Component {
        onChange: this.onChange
      };
 
-
-
+     
    return (
+     <section>
      <Autosuggest
          suggestions={suggestions}
          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
@@ -101,7 +102,9 @@ class SearchInput extends Component {
          getSuggestionValue={getSuggestionValue}
          renderSuggestion={renderSuggestion}
          inputProps={inputProps}
+         theme={theme}
        />
+       </section>
    );
 
  };
