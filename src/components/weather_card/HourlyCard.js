765c4d2d80ddddf6132cd952686b5f7c03dd67a2
devesh <devesh47cool@gmail.com>
2020-06-07 19:20:56 +0530

    Included template for hourly weather data

---
+++ src/components/weather_card/HourlyCard.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment, useEffect} from 'react';
+
+
+
+const HourlyCard = ({currentHour, address}) => {
+  // TODO: return card with feels like temp, weather name, time of day
+  // wind too maybe
+
+};
+
+
+export default HourlyCard;
