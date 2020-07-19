c2f0a826dd4590bb78c98005a9d0c1372d32f8fc
devesh <devesh47cool@gmail.com>
2020-07-19 18:27:08 +0530

    Updates

---
+++ precache-manifest.9a31b6acbcb47f554204b902290d9605.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4596360eeeea5e92745af83eb86e7a6",
+    "url": "/weather-man/index.html"
+  },
+  {
+    "revision": "d05e3a1961f428aa9edc",
+    "url": "/weather-man/static/css/main.b905e68b.chunk.css"
+  },
+  {
+    "revision": "ba1c53e740521eb2285e",
+    "url": "/weather-man/static/js/2.59d6529a.chunk.js"
+  },
+  {
+    "revision": "30d8ef3813de0269a8460a5317dd0eba",
+    "url": "/weather-man/static/js/2.59d6529a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d05e3a1961f428aa9edc",
+    "url": "/weather-man/static/js/main.2efc5014.chunk.js"
+  },
+  {
+    "revision": "3bd3c89f3bd6d5ebd02b",
+    "url": "/weather-man/static/js/runtime-main.4e7c9063.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/weather-man/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/weather-man/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/weather-man/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/weather-man/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/weather-man/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-man/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-man/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-man/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-man/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-man/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "d0288ce578853717bf92a0115850667e",
+    "url": "/weather-man/static/media/weatherman.d0288ce5.png"
+  }
+]);
