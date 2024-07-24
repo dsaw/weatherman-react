b81eb4e7736da485c531a93c8b046a3166aad08b
देवेश (devesh) <5353891+dsaw@users.noreply.github.com>
2024-07-23 19:09:32 -0600

    Update API.js
    changed proxy host

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === "production";
 // using cors-anywhere for the time being
 // localhost for testing the proxy locally
 const PREFIX = isProduction
-  ? `https://cors-proxy-serv.herokuapp.com/`
+  ? `https://cors-anywhere-x7ld.onrender.com/`
   : `http://localhost:8080/`;
 const WEATHER_API_SERVICE = "https://api.openweathermap.org/data/2.5/";
 //MetaWeather -> https://www.metaweather.com/api/
