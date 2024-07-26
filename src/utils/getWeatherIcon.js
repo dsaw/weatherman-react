2c745d3ed0c06117fee11a51983ad1c747085882
Devesh Sawant <devesh47cool@gmail.com>
2024-07-25 18:57:27 -0600

    Updates

--- src/utils/getWeatherIcon.js
+++
@@ -1,7 +0,0 @@
-// map url to icons
-const getWeatherIcon = (iconAbbr, size) => {
-  const sizeX = size !== "1" ? `@${size}x` : ``;
-  return `${iconAbbr}${sizeX}.png`;
-};
-
-export default getWeatherIcon;
--- src/utils/temperatureHelper.js
+++
@@ -1,9 +0,0 @@
-const convertToFahrenheit = (C) => {
-  return C * (9 / 5) + 32;
-};
-
-const convertToCelsius = (F) => {
-  return (F - 32) * (5 / 9);
-};
-
-export { convertToCelsius, convertToFahrenheit };
